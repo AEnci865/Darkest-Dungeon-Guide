46435fbf91a5937a8142d5cac7f4ab0bfc7116f7
AEnci865 <alexencinias96@gmail.com>
2026-03-30 13:50:47 -0400

    Update data.js

--- data.js
+++ data.js
@@ -3,28 +3,104 @@
 
 const REGIONS = {
 ruins: {
+teams: [
+{
+label: “Team A - High Damage”,
+4: { main:“Arbalest”, mS:[“Sniper Shot”,“Rallying Flare”,“Blindfire”,“Bola”], sub:“Highwayman”, sS:[“Point Blank Shot”,“Pistol Shot”,“Tracking Shot”,“Open Vein”] },
+3: { main:“Plague Doctor”, mS:[“Noxious Blast”,“Blinding Gas”,“Battlefield Medicine”,“Disorienting Blast”], sub:“Occultist”, sS:[“Wyrd Reconstruction”,“Weakening Curse”,“Hands from Below”,“Sacrificial Stab”] },
+2: { main:“Hellion”, mS:[“Wicked Hack”,“Iron Swan”,“If It Bleeds”,“YAWP!”], sub:“Bounty Hunter”, sS:[“Collect Bounty”,“Mark for Death”,“Flashbang”,“Finish Him”] },
+1: { main:“Crusader”, mS:[“Smite”,“Holy Lance”,“Stunning Blow”,“Inspiring Cry”], sub:“Flagellant”, sS:[“Punish”,“Rain of Sorrows”,“Redeem”,“Reclaim”] }
+},
+{
+label: “Team B - Balanced Sustain”,
 4: { main:“Vestal”, mS:[“Judgment”,“Dazzling Light”,“Divine Grace”,“Divine Comfort”], sub:“Occultist”, sS:[“Wyrd Reconstruction”,“Weakening Curse”,“Hands from Below”,“Sacrificial Stab”] },
-3: { main:“Plague Doctor”, mS:[“Noxious Blast”,“Blinding Gas”,“Battlefield Medicine”,“Disorienting Blast”], sub:“Jester”, sS:[“Inspiring Tune”,“Harvest”,“Solo”,“Slice Off”] },
-2: { main:“Highwayman”, mS:[“Duelist’s Advance”,“Point Blank Shot”,“Pistol Shot”,“Open Vein”], sub:“Houndmaster”, sS:[“Hound’s Rush”,“Whistle”,“Blackjack”,“Lick Wounds”] },
-1: { main:“Crusader”, mS:[“Smite”,“Holy Lance”,“Stunning Blow”,“Inspiring Cry”], sub:“Leper”, sS:[“Chop”,“Hew”,“Purge”,“Withstand”] }
+3: { main:“Occultist”, mS:[“Wyrd Reconstruction”,“Weakening Curse”,“Hands from Below”,“Sacrificial Stab”], sub:“Jester”, sS:[“Battle Ballad”,“Inspiring Tune”,“Slice Off”,“Harvest”] },
+2: { main:“Crusader”, mS:[“Smite”,“Holy Lance”,“Stunning Blow”,“Inspiring Cry”], sub:“Leper”, sS:[“Chop”,“Hew”,“Purge”,“Withstand”] },
+1: { main:“Man-at-Arms”, mS:[“Crush”,“Rampart”,“Defender”,“Retribution”], sub:“Leper”, sS:[“Chop”,“Hew”,“Purge”,“Withstand”] }
+},
+{
+label: “Team C - Antiquarian Loot”,
+4: { main:“Vestal”, mS:[“Judgment”,“Dazzling Light”,“Divine Grace”,“Divine Comfort”], sub:“Occultist”, sS:[“Wyrd Reconstruction”,“Weakening Curse”,“Hands from Below”,“Sacrificial Stab”] },
+3: { main:“Highwayman”, mS:[“Duelist’s Advance”,“Point Blank Shot”,“Pistol Shot”,“Tracking Shot”], sub:“Houndmaster”, sS:[“Hound’s Rush”,“Target Whistle”,“Guard Dog”,“Lick Wounds”] },
+2: { main:“Antiquarian”, mS:[“Flashpowder”,“Protect Me!”,“Nervous Stab”,“Invigorating Vapours”], sub:“Plague Doctor”, sS:[“Noxious Blast”,“Blinding Gas”,“Battlefield Medicine”,“Disorienting Blast”] },
+1: { main:“Man-at-Arms”, mS:[“Crush”,“Rampart”,“Defender”,“Bolster”], sub:“Crusader”, sS:[“Smite”,“Holy Lance”,“Stunning Blow”,“Inspiring Cry”] }
+}
+]
 },
 weald: {
-4: { main:“Arbalest”, mS:[“Sniper Shot”,“Flare”,“Rallying Flare”,“Bola”], sub:“Vestal”, sS:[“Divine Grace”,“Dazzling Light”,“Judgment”,“Divine Comfort”] },
-3: { main:“Houndmaster”, mS:[“Hound’s Rush”,“Cry Havoc”,“Guard Dog”,“Lick Wounds”], sub:“Occultist”, sS:[“Wyrd Reconstruction”,“Weakening Curse”,“Pull of the Abyss”,“Sacrificial Stab”] },
-2: { main:“Bounty Hunter”, mS:[“Collect Bounty”,“Mark for Death”,“Flashbang”,“Finish Him”], sub:“Abomination”, sS:[“Manacles”,“Beast’s Bile”,“Transform”,“Absolution”] },
-1: { main:“Hellion”, mS:[“Wicked Hack”,“Iron Swan”,“If It Bleeds”,“YAWP!”], sub:“Flagellant”, sS:[“Punish”,“Redeem”,“Rain of Sorrows”,“Reclaim”] }
+teams: [
+{
+label: “Team A - Bleed and Mark”,
+4: { main:“Plague Doctor”, mS:[“Noxious Blast”,“Blinding Gas”,“Battlefield Medicine”,“Disorienting Blast”], sub:“Arbalest”, sS:[“Sniper Shot”,“Rallying Flare”,“Blindfire”,“Bola”] },
+3: { main:“Grave Robber”, mS:[“Thrown Dagger”,“Pick to the Face”,“Shadow Fade”,“Lunge”], sub:“Highwayman”, sS:[“Duelist’s Advance”,“Point Blank Shot”,“Pistol Shot”,“Tracking Shot”] },
+2: { main:“Houndmaster”, mS:[“Hound’s Rush”,“Target Whistle”,“Cry Havoc”,“Lick Wounds”], sub:“Occultist”, sS:[“Wyrd Reconstruction”,“Weakening Curse”,“Pull of the Abyss”,“Sacrificial Stab”] },
+1: { main:“Bounty Hunter”, mS:[“Collect Bounty”,“Mark for Death”,“Flashbang”,“Finish Him”], sub:“Hellion”, sS:[“Wicked Hack”,“Iron Swan”,“If It Bleeds”,“YAWP!”] }
+},
+{
+label: “Team B - Control and Ranged”,
+4: { main:“Jester”, mS:[“Battle Ballad”,“Inspiring Tune”,“Slice Off”,“Harvest”], sub:“Vestal”, sS:[“Judgment”,“Dazzling Light”,“Divine Grace”,“Divine Comfort”] },
+3: { main:“Arbalest”, mS:[“Sniper Shot”,“Rallying Flare”,“Blindfire”,“Bola”], sub:“Occultist”, sS:[“Wyrd Reconstruction”,“Weakening Curse”,“Pull of the Abyss”,“Sacrificial Stab”] },
+2: { main:“Occultist”, mS:[“Wyrd Reconstruction”,“Weakening Curse”,“Pull of the Abyss”,“Sacrificial Stab”], sub:“Crusader”, sS:[“Smite”,“Holy Lance”,“Stunning Blow”,“Inspiring Cry”] },
+1: { main:“Man-at-Arms”, mS:[“Crush”,“Rampart”,“Defender”,“Bolster”], sub:“Houndmaster”, sS:[“Hound’s Rush”,“Target Whistle”,“Cry Havoc”,“Lick Wounds”] }
+},
+{
+label: “Team C - Antiquarian Loot”,
+4: { main:“Vestal”, mS:[“Judgment”,“Dazzling Light”,“Divine Grace”,“Divine Comfort”], sub:“Occultist”, sS:[“Wyrd Reconstruction”,“Weakening Curse”,“Pull of the Abyss”,“Sacrificial Stab”] },
+3: { main:“Highwayman”, mS:[“Duelist’s Advance”,“Point Blank Shot”,“Pistol Shot”,“Tracking Shot”], sub:“Bounty Hunter”, sS:[“Collect Bounty”,“Mark for Death”,“Flashbang”,“Finish Him”] },
+2: { main:“Houndmaster”, mS:[“Hound’s Rush”,“Target Whistle”,“Guard Dog”,“Lick Wounds”], sub:“Bounty Hunter”, sS:[“Collect Bounty”,“Mark for Death”,“Flashbang”,“Finish Him”] },
+1: { main:“Antiquarian”, mS:[“Flashpowder”,“Protect Me!”,“Nervous Stab”,“Invigorating Vapours”], sub:“Man-at-Arms”, sS:[“Crush”,“Rampart”,“Defender”,“Bolster”] }
+}
+]
 },
 warrens: {
-4: { main:“Occultist”, mS:[“Wyrd Reconstruction”,“Weakening Curse”,“Hands from Below”,“Pull of the Abyss”], sub:“Arbalest”, sS:[“Sniper Shot”,“Rallying Flare”,“Bandage”,“Bola”] },
-3: { main:“Jester”, mS:[“Battle Ballad”,“Inspiring Tune”,“Slice Off”,“Harvest”], sub:“Houndmaster”, sS:[“Hound’s Rush”,“Cry Havoc”,“Whistle”,“Lick Wounds”] },
-2: { main:“Houndmaster”, mS:[“Hound’s Rush”,“Blackjack”,“Cry Havoc”,“Guard Dog”], sub:“Abomination”, sS:[“Manacles”,“Beast’s Bile”,“Absolution”,“Transformation”] },
-1: { main:“Flagellant”, mS:[“Punish”,“Rain of Sorrows”,“Redeem”,“Reclaim”], sub:“Hellion”, sS:[“Wicked Hack”,“If It Bleeds”,“YAWP!”,“Adrenaline Rush”] }
+teams: [
+{
+label: “Team A - Beast Bleed Core”,
+4: { main:“Highwayman”, mS:[“Duelist’s Advance”,“Point Blank Shot”,“Pistol Shot”,“Tracking Shot”], sub:“Grave Robber”, sS:[“Thrown Dagger”,“Pick to the Face”,“Shadow Fade”,“Lunge”] },
+3: { main:“Plague Doctor”, mS:[“Noxious Blast”,“Blinding Gas”,“Battlefield Medicine”,“Disorienting Blast”], sub:“Occultist”, sS:[“Wyrd Reconstruction”,“Weakening Curse”,“Hands from Below”,“Pull of the Abyss”] },
+2: { main:“Flagellant”, mS:[“Punish”,“Rain of Sorrows”,“Redeem”,“Reclaim”], sub:“Bounty Hunter”, sS:[“Collect Bounty”,“Mark for Death”,“Flashbang”,“Finish Him”] },
+1: { main:“Houndmaster”, mS:[“Hound’s Rush”,“Target Whistle”,“Cry Havoc”,“Lick Wounds”], sub:“Hellion”, sS:[“Wicked Hack”,“Iron Swan”,“If It Bleeds”,“YAWP!”] }
+},
+{
+label: “Team B - Balanced Survivability”,
+4: { main:“Vestal”, mS:[“Judgment”,“Dazzling Light”,“Divine Grace”,“Divine Comfort”], sub:“Occultist”, sS:[“Wyrd Reconstruction”,“Weakening Curse”,“Hands from Below”,“Pull of the Abyss”] },
+3: { main:“Occultist”, mS:[“Wyrd Reconstruction”,“Weakening Curse”,“Hands from Below”,“Pull of the Abyss”], sub:“Plague Doctor”, sS:[“Noxious Blast”,“Blinding Gas”,“Battlefield Medicine”,“Disorienting Blast”] },
+2: { main:“Bounty Hunter”, mS:[“Collect Bounty”,“Mark for Death”,“Flashbang”,“Finish Him”], sub:“Houndmaster”, sS:[“Hound’s Rush”,“Target Whistle”,“Cry Havoc”,“Lick Wounds”] },
+1: { main:“Man-at-Arms”, mS:[“Crush”,“Rampart”,“Defender”,“Bolster”], sub:“Flagellant”, sS:[“Punish”,“Rain of Sorrows”,“Redeem”,“Reclaim”] }
+},
+{
+label: “Team C - Antiquarian Loot”,
+4: { main:“Jester”, mS:[“Battle Ballad”,“Inspiring Tune”,“Slice Off”,“Harvest”], sub:“Occultist”, sS:[“Wyrd Reconstruction”,“Weakening Curse”,“Hands from Below”,“Pull of the Abyss”] },
+3: { main:“Hellion”, mS:[“Wicked Hack”,“Iron Swan”,“If It Bleeds”,“YAWP!”], sub:“Highwayman”, sS:[“Duelist’s Advance”,“Point Blank Shot”,“Pistol Shot”,“Tracking Shot”] },
+2: { main:“Man-at-Arms”, mS:[“Crush”,“Rampart”,“Defender”,“Bolster”], sub:“Flagellant”, sS:[“Punish”,“Rain of Sorrows”,“Redeem”,“Reclaim”] },
+1: { main:“Antiquarian”, mS:[“Flashpowder”,“Protect Me!”,“Nervous Stab”,“Invigorating Vapours”], sub:“Houndmaster”, sS:[“Hound’s Rush”,“Target Whistle”,“Cry Havoc”,“Lick Wounds”] }
+}
+]
 },
 cove: {
-4: { main:“Vestal”, mS:[“Judgment”,“Divine Grace”,“Divine Comfort”,“Dazzling Light”], sub:“Occultist”, sS:[“Wyrd Reconstruction”,“Hands from Below”,“Weakening Curse”,“Pull of the Abyss”] },
-3: { main:“Plague Doctor”, mS:[“Noxious Blast”,“Incision”,“Battlefield Medicine”,“Disorienting Blast”], sub:“Occultist”, sS:[“Wyrd Reconstruction”,“Hands from Below”,“Weakening Curse”,“Sacrificial Stab”] },
+teams: [
+{
+label: “Team A - Blight and Control”,
+4: { main:“Arbalest”, mS:[“Sniper Shot”,“Rallying Flare”,“Blindfire”,“Bola”], sub:“Occultist”, sS:[“Wyrd Reconstruction”,“Weakening Curse”,“Hands from Below”,“Pull of the Abyss”] },
+3: { main:“Plague Doctor”, mS:[“Noxious Blast”,“Blinding Gas”,“Battlefield Medicine”,“Disorienting Blast”], sub:“Occultist”, sS:[“Wyrd Reconstruction”,“Weakening Curse”,“Hands from Below”,“Sacrificial Stab”] },
 2: { main:“Shieldbreaker”, mS:[“Pierce”,“Puncture”,“Snake Skin”,“Expose Weakness”], sub:“Highwayman”, sS:[“Duelist’s Advance”,“Point Blank Shot”,“Tracking Shot”,“Open Vein”] },
-1: { main:“Man-at-Arms”, mS:[“Crush”,“Rampart”,“Defender”,“Retribution”], sub:“Hellion”, sS:[“Wicked Hack”,“If It Bleeds”,“Iron Swan”,“YAWP!”] }
+1: { main:“Occultist”, mS:[“Wyrd Reconstruction”,“Weakening Curse”,“Hands from Below”,“Pull of the Abyss”], sub:“Man-at-Arms”, sS:[“Crush”,“Rampart”,“Defender”,“Retribution”] }
+},
+{
+label: “Team B - Stress and Damage”,
+4: { main:“Vestal”, mS:[“Judgment”,“Dazzling Light”,“Divine Grace”,“Divine Comfort”], sub:“Occultist”, sS:[“Wyrd Reconstruction”,“Weakening Curse”,“Hands from Below”,“Pull of the Abyss”] },
+3: { main:“Jester”, mS:[“Battle Ballad”,“Inspiring Tune”,“Slice Off”,“Harvest”], sub:“Plague Doctor”, sS:[“Noxious Blast”,“Blinding Gas”,“Battlefield Medicine”,“Disorienting Blast”] },
+2: { main:“Hellion”, mS:[“Wicked Hack”,“Iron Swan”,“If It Bleeds”,“YAWP!”], sub:“Shieldbreaker”, sS:[“Pierce”,“Puncture”,“Snake Skin”,“Expose Weakness”] },
+1: { main:“Flagellant”, mS:[“Punish”,“Rain of Sorrows”,“Redeem”,“Reclaim”], sub:“Houndmaster”, sS:[“Hound’s Rush”,“Target Whistle”,“Cry Havoc”,“Lick Wounds”] }
+},
+{
+label: “Team C - Antiquarian Loot”,
+4: { main:“Man-at-Arms”, mS:[“Crush”,“Rampart”,“Defender”,“Bolster”], sub:“Jester”, sS:[“Battle Ballad”,“Inspiring Tune”,“Slice Off”,“Harvest”] },
+3: { main:“Occultist”, mS:[“Wyrd Reconstruction”,“Weakening Curse”,“Hands from Below”,“Pull of the Abyss”], sub:“Plague Doctor”, sS:[“Noxious Blast”,“Blinding Gas”,“Battlefield Medicine”,“Disorienting Blast”] },
+2: { main:“Highwayman”, mS:[“Duelist’s Advance”,“Point Blank Shot”,“Pistol Shot”,“Tracking Shot”], sub:“Grave Robber”, sS:[“Thrown Dagger”,“Pick to the Face”,“Shadow Fade”,“Lunge”] },
+1: { main:“Antiquarian”, mS:[“Flashpowder”,“Protect Me!”,“Nervous Stab”,“Invigorating Vapours”], sub:“Man-at-Arms”, sS:[“Crush”,“Rampart”,“Defender”,“Bolster”] }
+}
+]
 }
 };
 const BOSSES = [
